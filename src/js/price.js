7adb79775e9ab8ce0035a1ed432a6f02007c5325
Tom Scanlan <tompscanlan@gmail.com>
2024-08-10 18:23:38 -0400

    handle price

---
+++ src/js/price.js
@@ -0,0 +1,17 @@
+console.log('price.js loaded');
+
+var t = TrelloPowerUp.iframe();
+
+window.estimate.addEventListener("submit", function (event) {
+  // Stop the browser trying to submit the form itself.
+  event.preventDefault();
+  return t
+    .set("card", "shared", "price", window.price.value)
+    .then(function () {
+      t.closePopup();
+    });
+});
+
+t.render(function () {
+  t.sizeTo("#price").done();
+});
