ea2dec8eea7f8020fe848c439af723c514b30f42
Tom Scanlan <tompscanlan@gmail.com>
2024-08-10 17:27:20 -0400

    relocate to get static image in proper place

--- src/js/connector.js
+++ src/js/connector.js
@@ -2,6 +2,13 @@ console.log('connector.js loaded');
 window.TrelloPowerUp.initialize({
     'card-badges': function(t, opts) {
         // return array of card badges for a given card
-        return [];
+        return t.card('all')
+        .then(function(card) {
+            console.log(card);
+            return [ {
+                text: card.name,
+                color: 'blue'
+            }];
+        });
     }
 });
